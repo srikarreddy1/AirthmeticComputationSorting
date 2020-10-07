13151feb10d3a888007fb47ee0f476ddf6637b2c
unknown <fddxzf@gmail.com>
2020-10-07 12:29:02 +0530

    code to compute a%b+c added

--- ArithmeticComputationSorting.sh
+++ ArithmeticComputationSorting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
- 	
-	echo "Airthmetic operations and store them"
+
+   echo "Airthmetic operations and store them"
    #read the a b c
    read -p "enter the a b c values:" a b c
    #compute a+b*c
@@ -10,9 +10,9 @@
    result1=$(($a*$b+$c))
    echo "$result1"
    #compute C+a/b 
-   result2=$((c+a/b))
+   result2=$(($c+$a/$b))
    echo "$result2"
-   
+   result3=$(($a%$b+$c))
+   echo "$result3"
 
-   
 
